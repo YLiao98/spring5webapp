9d866b889723fda4d98c30b45b89c9cccbc59b2d
YLiao98 <hsyliao@gmail.com>
2021-11-10 21:21:03 -0500

    Assignment 2 - Add Publisher Entity, create Publisher in BootStrap class

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -15,10 +17,12 @@ public class BootStrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -40,7 +44,17 @@ public class BootStrapData implements CommandLineRunner {
         authorRepository.save(rod);
         bookRepository.save(noEJB);
 
+        Publisher pub1 = new Publisher("foo", "add1", "city1","state1","zip1");
+        Publisher pub2 = new Publisher("foo2", "add2", "city2","state2","zip2");
+        Publisher pub3 = new Publisher("foo3", "add3", "city3","state3","zip3");
+        Publisher pub4 = new Publisher("foo4", "add4", "city4","state4","zip4");
+
+        publisherRepository.save(pub1);
+        publisherRepository.save(pub2);
+        publisherRepository.save(pub3);
+        publisherRepository.save(pub4);
         System.out.println("Started in Bootstrap");
         System.out.println("Number of Books: " + bookRepository.count());
+        System.out.println("Number of publishers: "+ publisherRepository.count());
     }
 }
